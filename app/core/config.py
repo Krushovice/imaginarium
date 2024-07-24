3cebef366607848c6fbc700054641c0831788d54
Krushovice <kickstar69@yandex.ru>
2024-07-24 18:10:25 +1000

    install pydantic_settings

---
+++ app/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROD_DB_URL: str
+
+    @property
+    def get_db_url(self) -> str:
+        return self.PROD_DB_URL
+
+    model_config = SettingsConfigDict(env_file="../../.env")
+
+
+settings = Settings()
