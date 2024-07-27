c1127d35cf0ce83040762f9b598cb247c1eb8057
Krushovice <kickstar69@yandex.ru>
2024-07-27 12:44:46 +1000

    create user model

---
+++ app/api/orm/models/user.py
@@ -0,0 +1,21 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from api.orm import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+    username: Mapped[str] = mapped_column(
+        String(50), unique=True, index=True, nullable=False
+    )
+    email: Mapped[str] = mapped_column(
+        String(50), unique=True, index=True, nullable=False
+    )
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.username!r}, fullname={self.first_name!r}{self.last_name!r})"
--- app/orm/models/user.py
+++
@@ -1,5 +0,0 @@
-from orm import Base
-
-
-class User(Base):
-    pass
