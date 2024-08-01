844daac5e79622f8f4d402d43dc67da179bf8881
Krushovice <kickstar69@yandex.ru>
2024-08-01 18:52:21 +1000

    create new methods in db_healper for easy work with session

--- app/api/orm/db_helper.py
+++ app/api/orm/db_helper.py
@@ -1,5 +1,10 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from api.orm import Base
+from sqlalchemy.ext.asyncio import (
+    async_sessionmaker,
+    create_async_engine,
+    async_scoped_session,
+    AsyncSession,
+)
+from asyncio import current_task
 from api.core.config import settings
 
 
@@ -15,6 +20,20 @@ class DataBaseHelper:
             autocommit=False,
             expire_on_commit=False,
         )
+
+    # метод для получения актуальной сессии
+    def get_scope_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    # метод для создания сессии для каждого запроса и удаления ее после
+    async def session_dependency(self) -> AsyncSession:
+        async with self.get_scope_session() as session:
+            yield session
+            await session.remove()
 
 
 db_helper = DataBaseHelper(
