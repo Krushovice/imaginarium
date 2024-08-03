9d7b3f1ab96c304409ed7e3d7b9d06eb7931ac0c
Krushovice <kickstar69@yandex.ru>
2024-08-03 11:57:26 +1000

    add relationship between user and review

--- app/api/orm/models/review.py
+++ app/api/orm/models/review.py
@@ -1,8 +1,11 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column
-
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Review(Base):
@@ -11,6 +14,8 @@ class Review(Base):
     body: Mapped[str] = mapped_column(String(100))
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="reviews")
 
     def __repr__(self) -> str:
         return f"Review(id={self.id!r}, title={self.title!r}, user_id={self.user_id!r})"
--- app/api/orm/models/user.py
+++ app/api/orm/models/user.py
@@ -1,8 +1,12 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
 
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .review import Review
 
 
 class User(Base):
@@ -15,6 +19,8 @@ class User(Base):
     email: Mapped[str] = mapped_column(
         String(50), unique=True, index=True, nullable=False
     )
+
+    reviews: Mapped[list["Review"]] = relationship(back_populates="user")
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, fullname={self.first_name!r}{self.last_name!r})"
