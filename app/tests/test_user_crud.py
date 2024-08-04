6e81e66baeaaaf035593f8566ac5c41c1601c6c9
Krushovice <kickstar69@yandex.ru>
2024-08-04 16:35:49 +1000

    install pytest

--- app/tests/test_user_crud.py
+++ app/tests/test_user_crud.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.orm import db_helper, User, Profile, Review
@@ -20,13 +21,24 @@ async def create_user(
     return user
 
 
+async def get_user_by_username(
+    session: AsyncSession,
+    username: str,
+) -> User | None:
+    stmt = select(User).where(User.username == username)
+    user: User | None = await session.scalar(stmt)
+    print("user", user)
+    return user
+
+
 async def main():
     async with db_helper.session_factory() as session:
         await create_user(
             session=session,
-            username="Krush",
-            email="krushovice@yahoo.com",
+            username="Anna",
+            email="Anna@yahoo.com",
         )
+        await get_user_by_username(session=session, username="Krush")
 
 
 if __name__ == "__main__":
