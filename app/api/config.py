96a08adff098abe101ca3c729f6a58f42bd16c2a
Krushovice <kickstar69@yandex.ru>
2024-08-05 17:45:11 +1000

    fix config db path

--- app/api/config.py
+++ app/api/config.py
@@ -3,11 +3,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 
-BASE_DIR = Path(__file__).cwd().parent
+BASE_DIR = Path(__file__).parent.parent.parent
+DB_PATH = BASE_DIR / "db.sqlite3"
 
 
 class DbConfig(BaseModel):
-    url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
     echo: bool = True
 
 
